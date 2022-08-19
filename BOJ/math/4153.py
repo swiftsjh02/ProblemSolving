7f40d28cabbfe2d103d6688d0b49d1a2e657cc33
ssohye <ssohye@icloud.com>
2022-08-19 17:22:54 +0900

    complete

---
+++ BOJ/math/4153.py
@@ -0,0 +1,11 @@
+
+
+while True:
+    a=[k for k in input().split(" ")]
+    if a[0]==0:
+        break
+
+    if int(a[0])**2+int(a[1])**2==int(a[2])**2:
+        print("right")
+    else:
+        print("wrong")
