e48b7caca7cfbb6854e44cba72fc9508a17ff33c
ssohye <ssohye@icloud.com>
2022-08-19 13:33:46 +0900

    completes

--- BOJ/math/1419.py
+++ BOJ/math/1419.py
@@ -4,8 +4,8 @@ k=int(input())
 
 
 s1=set()
-for x in range(1,1000000000):
-    for d in range(1,100000000):
+for x in range(1,10):
+    for d in range(1,10):
         if k*(x+int(k*d-d/2))>=l and k*(x+int(k*d-d/2))<=r:
             s1.add(k*(x+int(k*d-d/2)))
 
