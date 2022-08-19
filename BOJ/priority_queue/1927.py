d4b3ef4d2385adf52b67621dcb7105074db254ca
ssohye <ssohye@icloud.com>
2022-08-19 10:43:32 +0900

    complete

---
+++ BOJ/priority_queue/1927.py
@@ -0,0 +1,14 @@
+
+from heapq import heappop,heappush
+import sys
+
+
+heaps=[]
+for i in range(int(sys.stdin.readline())):
+    n=int(sys.stdin.readline())
+    if n!=0:
+        heappush(heaps,n)
+    elif len(heaps)!=0:
+        print(heappop(heaps))
+    else:
+        print(0)
