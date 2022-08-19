8f13e7c7c8a9f117dda8ebb3d38cb7a6a34acd1b
ssohye <ssohye@icloud.com>
2022-08-19 11:38:47 +0900

    complete

---
+++ BOJ/dynamic_programming/2748.py
@@ -0,0 +1,19 @@
+
+
+def fibo(n):
+    global fibolist
+    if n<=2:
+        return 1
+    if fibolist[n]!=0:
+        return fibolist[n]
+    fibolist[n]=fibo(n-1)+fibo(n-2)
+    return fibolist[n]
+    
+
+
+
+n=int(input())
+fibolist=[0]*(n+1)
+
+
+print(fibo(n))
