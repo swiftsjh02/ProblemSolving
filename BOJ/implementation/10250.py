341c8674efdc3e58561bf9aaa360b919b96b0fb7
ssohye <ssohye@icloud.com>
2022-08-21 15:30:31 +0900

    complete

--- BOJ/implementation/10250.py
+++ BOJ/implementation/10250.py
@@ -1,4 +1,7 @@
 
+
+import math
+
 for i in range(int(input())):
     a=[]
     info=[int(k) for k in input().split()]
@@ -8,6 +11,16 @@ for i in range(int(input())):
         for i in range(start,start+info[1]):
             tmp.append(i)
         a.append(tmp)
-    shift=info[2]//info[0]
+
+
+    
+    
+    shift=math.ceil(info[2]/info[0])-1 #2
     flo=info[2]%info[0]
+    if flo==0:
+        flo=info[0]
+
+  
     print(a[flo-1][shift])
+
+    
