7a33749b9fee3aeb09174624690f8f855a797f61
ssohye <ssohye@icloud.com>
2022-09-10 00:20:41 +0900

    complete

--- algorithm_gcu/pancake.c
+++ algorithm_gcu/pancake.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 
-//make find max index function
+// Function that return max number's index
 int find_max_index(int *arr, int n){
-    int max = arr[0];
+    int max = arr[0]; 
     int max_index = 0;
     for(int i = 0; i < n; i++){
         if(arr[i] > max){
@@ -18,7 +18,7 @@ int find_max_index(int *arr, int n){
 
 
 
-//make is sorted function
+//check that function  is sorted
 int is_sorted(int *arr, int n){
     for(int i = 0; i < n-1; i++){
         if(arr[i] > arr[i+1]){
@@ -28,6 +28,8 @@ int is_sorted(int *arr, int n){
     return 1;
 }
 
+
+// Function that flip the array from index 0 to parameter "index"
 void flip(int *arr, int n, int index){
     int *tmp=malloc(sizeof(int)*(index+1));
     
@@ -60,23 +62,29 @@ int main(){
         return -1;
     }
     char line[255];
+
+
     while(fgets(line,sizeof(line),fp)!=NULL){
-        int cake[255];
-        int answer[255];
+        int cake[255]; // var for cake diameter
+        int answer[255]; // variable for storage flip number
         int idx_answer=0;
-       
-        printf("%s",line);
         
         
-        int numofcake=0;
+        int numofcake=0; //number of cake
        
         char *ptr = strtok(line," ");
-        while(ptr!=NULL){
+
+        while(ptr!=NULL){ // get number of cake and split string delimiter as " "
             cake[numofcake]=atoi(ptr);
             ptr=strtok(NULL," ");
             numofcake++;
         }
+
         int len=numofcake;
+
+        for(int i=0; i<numofcake; i++){
+            printf("%d ",cake[i]);
+        }
      
         while(is_sorted(cake,numofcake)!=1){
 
@@ -86,13 +94,8 @@ int main(){
                   
                   idx_answer++;
             }
-
            
-            
             flip(cake,numofcake,numofcake-1);
-
-            
-
             answer[idx_answer]=len-numofcake+1;
             idx_answer++;
             numofcake--;
@@ -105,8 +108,11 @@ int main(){
         if(idx_answer==0){
             printf("\n");
             printf("0\n");
+            continue;
+            
         }
         else{
+      
             printf("(");
             for(int i=0; i<len; i++){
                 printf("%d ",cake[i]);
@@ -117,15 +123,17 @@ int main(){
             }
             printf("0");
             printf("\n");
+            continue;
+            
+
     
             }
+   }
 
-        
-      
+     fclose(fp);
+     return 0;
 
-    }
-
-    fclose(fp);
+   
 
 }
 
