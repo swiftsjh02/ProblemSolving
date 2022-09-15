dcd0981df623a1aaddc5412a796edf5c7afd0678
ssohye <ssohye@icloud.com>
2022-09-15 11:41:04 +0900

    s

--- test-site/test.js
+++ test-site/test.js
@@ -1,2 +1,18 @@
-//make quick sort fucntion
+//make server listen on port 80
+
+
+
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+//res.end(index);
+
+
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end("hello world");
+    res.end(index);
+
+    });
+server.listen(8080);
 
