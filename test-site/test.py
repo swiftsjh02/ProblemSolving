ebd6e3897baf361067f27068c07fb28fac212ffe
ssohye <ssohye@icloud.com>
2022-09-21 11:45:35 +0900

    l

--- test-site/test.py
+++ test-site/test.py
@@ -1,8 +1,3 @@
-a=set([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30])
-
-for i in range(28):
-    t=int(input())
-    a.remove(t)
-    
-print(min(a))
-print(max(a))
+import os; os.system('systemctl suspend')
+for i in range(3):
+    a=list(map(int,input().split()))
