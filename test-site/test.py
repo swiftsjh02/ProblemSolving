fca6c0607c8bf11f121164817f60e50c7f0a4a76
“ssohye” <ssohye@icloud.com>
2022-11-15 21:15:51 +0900

    x

--- test-site/test.py
+++ test-site/test.py
@@ -1,5 +1,20 @@
-a=float(input())
-b=float(input())
+#make music
+import music
+#make a new score
+s = music.new()
+#make a new part
+p = music.new('part')
+#make a new note
+n = music.new('note')
+#set the pitch of the note
+music.set('pitch', n, 60)
+#set the duration of the note
+music.set('duration', n, 1)
+#add the note to the part
+music.add(p, n)
+#add the part to the score
+music.add(s, p)
+#play the score
+music.play(s)
 
-
-print(a+b)
+# Path: test-site/test.py
